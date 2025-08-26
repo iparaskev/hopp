a05b71867bb86b39c2856cacff4afbbdb9939082
Iason Paraskevopoulos <iaswnparaskev@gmail.com>
2025-08-26 16:55:16 +0100

    test

--- tauri/src-tauri/src/main.rs
+++ tauri/src-tauri/src/main.rs
@@ -28,7 +28,7 @@ use std::time::Duration;
 #[cfg(any(target_os = "windows", target_os = "linux"))]
 use tauri::PhysicalPosition;
 
-//test again
+//test
 #[tauri::command]
 async fn screenshare(
     app: tauri::AppHandle,
